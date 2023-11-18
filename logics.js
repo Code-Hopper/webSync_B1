d2c9375f276102c3f19e37674cb9a2d5498e08e4
code_hopper <ameykhondekar01@gmail.com>
2023-11-18 08:33:59 +0530

    first js pratical class

--- logics.js
+++ logics.js
@@ -1,1 +1,35 @@
 // This is first js class 
+
+// alert("External JS")
+
+// variables
+
+// scope variable_name = data;
+
+// var public { let const } protected
+
+// var a = 10;
+
+let a = 10;
+
+let b = 20;
+
+// let c = a + b;
+
+const c = a + b;
+
+a = "Amey";
+
+// statements & expresions
+
+// output function
+// this are pre-defined function to prompt the output of something
+
+// alert, console.log, document.write
+
+// alert(c)
+
+// console.log(c)
+
+// document.write(c)
+
