c43deb66225fa8f885238a5ec3d84bcb4e961c95
code_hopper <ameykhondekar01@gmail.com>
2023-11-18 08:52:16 +0530

    datatypes

--- logics.js
+++ logics.js
@@ -33,3 +33,49 @@ a = "Amey";
 
 // document.write(c)
 
+// datatypes
+
+// primitive data types 
+
+// number , string , boolean, null, undefined
+
+// non-primitive data types
+
+// array , objects, function
+
+// number -Infinity to Infinity
+
+        // integers - 22 1 22 5 77 98 63 
+        // floats - 2.1 2 2.3 2.0 54684 35468 3434
+
+let x = 123456
+
+console.log(x)
+// typeof(x) // this function does not return the output we must put it under some output function 
+console.log(typeof(x))
+
+// String 
+
+// collection of charchter , that can be just alphabets / number / alphanumeric / this do content symbols 
+
+// string has to be written in qoutes either '' or ""
+
+let y =  "Some string value 12345"
+
+console.log(y);
+console.log(typeof(y))
+
+// Boolean
+
+// either true or flase
+
+let z = true;
+
+let w = false;
+
+console.log(z)
+console.log(typeof(z))
+
+
+console.log(w)
+console.log(typeof(w))
