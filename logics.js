bc0c105b27839816726a706bd2ba64154ffc35a7
code_hopper <ameykhondekar01@gmail.com>
2023-11-20 08:34:40 +0530

    this is 2 nd class

--- logics.js
+++ logics.js
@@ -10,15 +10,15 @@
 
 // var a = 10;
 
-let a = 10;
+// let a = 10;
 
-let b = 20;
+// let b = 20;
 
-// let c = a + b;
+// // let c = a + b;
 
-const c = a + b;
+// const c = a + b;
 
-a = "Amey";
+// a = "Amey";
 
 // statements & expresions
 
@@ -45,14 +45,14 @@ a = "Amey";
 
 // number -Infinity to Infinity
 
-        // integers - 22 1 22 5 77 98 63 
-        // floats - 2.1 2 2.3 2.0 54684 35468 3434
+// integers - 22 1 22 5 77 98 63 
+// floats - 2.1 2 2.3 2.0 54684 35468 3434
 
-let x = 123456
+// let x = 123456
 
-console.log(x)
+// console.log(x)
 // typeof(x) // this function does not return the output we must put it under some output function 
-console.log(typeof(x))
+// console.log(typeof(x))
 
 // String 
 
@@ -60,22 +60,109 @@ console.log(typeof(x))
 
 // string has to be written in qoutes either '' or ""
 
-let y =  "Some string value 12345"
+// let y =  "Some string value 12345"
 
-console.log(y);
-console.log(typeof(y))
+// console.log(y);
+// console.log(typeof(y))
 
-// Boolean
+// // Boolean
 
-// either true or flase
+// // either true or flase
 
-let z = true;
+// let z = true;
 
-let w = false;
+// let w = false;
 
-console.log(z)
-console.log(typeof(z))
+// console.log(z)
+// console.log(typeof(z))
 
 
-console.log(w)
-console.log(typeof(w))
+// console.log(w)
+// console.log(typeof(w))
+
+
+// let a = Number(prompt("Enter some value"))
+
+// // 1 9456653453 45343545
+// // "1a" 1a - NaN
+// // let a = prompt("Enter some value")
+
+// console.log(typeof(a)) // NaN
+// console.log(a)
+
+// object
+
+// let student = {
+//         roll: 1,
+//         class: "A1",
+//         subject: "Maths"
+// }
+
+// console.log(student.roll)
+
+// console.log(student.class)
+
+// console.log(student.subject)
+
+// console.log(typeof (student))
+
+// array 
+
+// collection of similar/same type data
+
+// let arr = [ element, element ,element,element, ... ]
+
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+// console.log(nums)
+// console.log(typeof(nums))
+
+// let class_a1 = [
+//         {
+//                 roll: 1,
+//                 class: "A1",
+//                 subject: "Maths"
+//         },
+//         {
+//                 roll: 2,
+//                 class: "A1",
+//                 subject: "Maths"
+//         },
+//         {
+//                 roll: 3,
+//                 class: "A1",
+//                 subject: "Maths"
+//         },
+//         {
+//                 roll: 4,
+//                 class: "A1",
+//                 subject: "Maths"
+//         }
+// ] 
+
+// string concatation
+
+// let my_name = prompt("Enter some name");
+
+// let my_age = prompt("Enter your age");
+
+// let some_data = "Hello world !, my name is " + my_name + ". My age is "+ my_age + "." 
+
+// console.log("Hello world !, my name is " + my_name + ". My age is "+ my_age + "." )
+
+// document.write("Hello world !, my name is " + my_name + ". My age is "+ my_age + "." )
+
+// alert("Hello world !, my name is " + my_name + ". My age is "+ my_age + "." )
+
+// console.log(some_data)
+// console.log(my_name)
+
+// oprators
+
+// 1.Arithmatic Oprators + - * / %
+// 2.Increment & Decrement ++ -- , pre ++a , post a++, pre --a, post a--
+// 3.Conditonal Oprators < > <= >= != == ===
+// 4.Logical Oprators & || !
+// 5.Relarional Oprators .
+// 6.Assignment Oprators =
+// 7.Ternary Oparator ?
